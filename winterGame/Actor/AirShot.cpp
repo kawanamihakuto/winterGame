cca0f368d71b40c1c156d42d038d2296c2f90286
kawanamihakuto <fko2547026@stu.o-hara.ac.jp>
2026-01-28 22:27:17 +0900

    コード修正

--- winterGame/Actor/AirShot.cpp
+++ winterGame/Actor/AirShot.cpp
@@ -5,11 +5,16 @@
 #include"Enemy/WalkEnemy.h"
 #include<DxLib.h>
 
+//ãÛãCíeÇÃêÿÇËéÊÇËà íu
 constexpr float kAirGraphCutRow = 5;
+//
 constexpr float kSpeed = 6.0f;
-constexpr int kWidth = 16;
-constexpr int kHeight = 16;
-constexpr int kSize = 1;
+constexpr int kWidth = 32;
+constexpr int kHeight = 32;
+constexpr int kSrcWidth = 48;
+constexpr int kSrcHeight = 32;
+constexpr int kSrcSize = 16;
+constexpr double kSize = 1.0;
 constexpr int kCountMax = 20;
 
 AirShot::AirShot(bool isRight,Vector2 pos, int graphHandle, std::shared_ptr<EffectManager>effectManager):Shot(isRight,pos,graphHandle,effectManager),
@@ -65,13 +70,13 @@ void AirShot::Draw(Camera& camera)
 {
 	Vector2 screen = camera.WorldToScreen(position_);
 	DrawRectRotaGraph(screen.x, screen.y,
-		0, kHeight * kAirGraphCutRow, kWidth * 3, kHeight * 2,
+		0, kSrcSize * kAirGraphCutRow, kSrcWidth, kSrcHeight,
 		kSize, 0, graphHandle_, true, !isRight_);
 #ifdef _DEBUG
 	Rect drawRect = rect_;
 	//ìñÇΩÇËîªíËï\é¶
 	drawRect.SetCenter(screen.x, screen.y,
-		kWidth * 2 * kSize, kHeight* 2 * kSize);
+		kWidth, kHeight);
 	drawRect.Draw(0x00aaff, false);
 #endif // _DEBUG
 }
